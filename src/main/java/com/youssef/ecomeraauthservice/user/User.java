25f2c4f402ca167d0cfb01632984d57b1cfd0f9d
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-03-22 18:46:55 +0100

    feat: Add User entity & repository with Role and Permission enums

---
+++ src/main/java/com/youssef/ecomeraauthservice/user/User.java
@@ -0,0 +1,71 @@
+package com.youssef.ecomeraauthservice.user;
+
+import com.youssef.ecomeraauthservice.common.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Getter
+@Setter
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "app_user")
+public class User extends BaseEntity implements UserDetails {
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 100)
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 100)
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    @Size(max = 255)
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    @Column
+    private LocalDateTime lastLogin;
+
+    @Size(max = 45)
+    @Column(length = 45)
+    private String ipAddress;
+
+
+    // Spring Security UserDetails methods
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
